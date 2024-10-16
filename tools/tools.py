be6be48a0e6492752e92eac1f98b9e51c859a9eb
Caterina Gazzotti <gazzotti.caterina@gmail.com>
2024-10-16 07:57:49 +0000

    Add new functions

--- tools/tools.py
+++ tools/tools.py
@@ -16,3 +16,39 @@ def describe_country(country_name:str)->str:
         print(f"HTTP error occurred: {http_err}")
     except Exception as err:
         print(f"Other error occurred: {err}")
+
+
+####TODO: SHOULD BE THE SAME INITIALIZE() BUT WITH CUSTOM MESSAGES!!!
+def initialize(group_manager:ConversableAgent, first_agent:ConversableAgent):
+
+    first_agent.initiate_chat(
+        group_manager,
+        max_round = 6,
+        message = "Let's start with paper 10.1126/scitranslmed.abb3107. Please get its title and abstract. After that, save the following review to the existing paper with DOI 10.1126/scitranslmed.abb3107: Xiaojun has reviewed the paper (DOI: 10.1126/scitranslmed.abb3107) and judges that is a paper about PSC, and she commented that it is one of the primary work in this field. For this paper, no need to ask an additional opinion on whether it is about PSC or not."
+    )
+
+def expand_references(group_manager:ConversableAgent, first_agent:ConversableAgent):
+    first_agent.initiate_chat(
+        group_manager,
+        max_round = 6,
+        message = "Your task is as follows: "
+            "1. Please query from the notes (the knowledge graph) to find one paper"
+            "which has been Reviewed as YES for PSC research, and get its DOI."
+            "2. If you find such as paper, retrieve all its citations, as a list of DOIs."
+            "3. For each DOI in the citation list, create a Paper node in the knowledge graph, "
+            "with only the property of DOI, and create a 'Cites' relation from the original paper (matched by its DOI) to the newly created paper. "
+            "Since there will normally be many citations, "
+            "please use a batch of data and the UNWIND keyword to create/merge these nodes in one cypher query."
+    )
+
+def review_paper(group_manager:ConversableAgent, first_agent:ConversableAgent):
+    first_agent.initiate_chat(
+        group_manager,
+        max_round = 6,
+        message = "Your task is as follows: "
+            "1. Query your note (the graph) to find one paper which has not been reviewed. Done for the graph tool."
+            "2. Use another tool to retrieve the title and abstract of this paper, and save the details into the graph."
+            "3. Ask the psc_checker to see if it is about psc"
+            "4. Save the Review about this paper, with YES/NO and comment, and record that this review is done by generative AI"
+    )
+

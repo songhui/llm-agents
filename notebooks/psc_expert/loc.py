a82fa4186eff32e95b873f2362a5a171cdd0468e
Hui Song <hui.song@sintef.no>
2024-11-07 14:43:07 +0100

    a demo run

---
+++ notebooks/psc_expert/loc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from json import load
+from sys import argv
+
+def loc(nb):
+    cells = load(open(nb))['cells']
+    return sum(len(c['source']) for c in cells if c['cell_type'] == 'code')
+
+def run(ipynb_files):
+    return sum(loc(nb) for nb in ipynb_files)
+
+if __name__ == '__main__':
+    print(argv[1:])
+    print(run(argv[1:]))
